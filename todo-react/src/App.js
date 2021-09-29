1367ce2ebddc5bab1febaac50495d82d138271c0
Ellen Park <ellenpark781@gmail.com>
2021-09-29 13:17:10 -0400

    creating item component as well as initial state

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -1,11 +1,30 @@
+import { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItem: "",
+      list : []
+    }
+  }
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <h1>Add an Item</h1>
+        <input
+          type= "text"
+          placeholder = "Type item here..."
+          value = {this.state.newItem}
+          onChange = {e => this.updateItem("newItem", e.target.value)}
+        />
+      </div>
+    );
+  }
+  
 }
+
 
 export default App;
